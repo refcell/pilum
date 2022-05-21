a054a610a3055aaaade6443adcfda7521f6d84d2
Andreas Bigger <abigger87@gmail.com>
2022-05-21 08:06:33 -0400

    verbose output

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -2,15 +2,34 @@
 
 ls -lsa
 
-node > package.json <<EOF
-//Read data
+node <<EOF
+// Read data
 var data = require('./package.json');
+console.log('Reade package.json');
+console.log(data);
 
-//Manipulate data
+// Update package registry info
 data.name = '@abigger87/pilum';
 data.publishConfig.registry = 'https://npm.pkg.github.com';
 
-//Output data
+console.log('Updated variables in package.json');
+
+// Output data
+console.log(JSON.stringify(data));
+
+EOF
+
+
+
+node > package.json <<EOF
+// Read data
+var data = require('./package.json');
+
+// Update package registry info
+data.name = '@abigger87/pilum';
+data.publishConfig.registry = 'https://npm.pkg.github.com';
+
+// Output data
 console.log(JSON.stringify(data));
 
 EOF
