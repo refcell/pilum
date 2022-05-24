2693c5559c6ea1d7dd8298fb7818f310f825f1ac
Andreas Bigger <abigger87@gmail.com>
2022-05-24 07:36:30 -0400

    bundling

---
+++ rollup.config.ts
@@ -0,0 +1,151 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import { terser } from 'rollup-plugin-terser';
+import json from 'rollup-plugin-json';
+import commonjs from 'rollup-plugin-commonjs';
+import bundleSize from 'rollup-plugin-bundle-size';
+
+import pkg from './package.json';
+
+const extensions = ['.js'];
+const babelRuntimeVersion = pkg.dependencies['@babel/runtime'].replace(
+  /^[^0-9]*/,
+  ''
+);
+
+const makeExternalPredicate = (externalArr) => {
+  if (externalArr.length === 0) {
+    return () => false;
+  }
+  const pattern = new RegExp(`^(${externalArr.join('|')})($|/)`);
+  return (id) => pattern.test(id);
+};
+
+export default [
+  // CommonJS
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/multicall.cjs.js',
+      format: 'cjs',
+      indent: false,
+      sourcemap: true,
+    },
+    external: makeExternalPredicate([...Object.keys(pkg.dependencies || {})]),
+    plugins: [
+      json(),
+      nodeResolve({
+        extensions,
+      }),
+      babel({
+        extensions,
+        plugins: [
+          ['@babel/plugin-transform-runtime', { version: babelRuntimeVersion }],
+        ],
+        runtimeHelpers: true,
+      }),
+      bundleSize(),
+    ],
+  },
+
+  // ESM
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/multicall.esm.js',
+      format: 'es',
+      indent: false,
+      sourcemap: true,
+    },
+    external: makeExternalPredicate([...Object.keys(pkg.dependencies || {})]),
+    plugins: [
+      json(),
+      nodeResolve({
+        extensions,
+      }),
+      babel({
+        extensions,
+        plugins: [
+          [
+            '@babel/plugin-transform-runtime',
+            { version: babelRuntimeVersion, useESModules: true },
+          ],
+        ],
+        runtimeHelpers: true,
+      }),
+      bundleSize(),
+    ],
+  },
+
+  // ESM for Browsers
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/multicall.esm.mjs',
+      format: 'es',
+      indent: false,
+      sourcemap: true,
+    },
+    plugins: [
+      json(),
+      nodeResolve({
+        extensions,
+        browser: true,
+      }),
+      commonjs(),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      }),
+      babel({
+        extensions,
+        exclude: 'node_modules/**',
+      }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          // warnings: false,
+        },
+      }),
+      bundleSize(),
+    ],
+  },
+
+  // UMD
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/multicall.umd.js',
+      format: 'umd',
+      name: 'Multicall',
+      indent: false,
+      sourcemap: true,
+    },
+    plugins: [
+      json(),
+      nodeResolve({
+        extensions,
+        browser: true,
+      }),
+      commonjs(),
+      babel({
+        extensions,
+        exclude: 'node_modules/**',
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          // warnings: false,
+        },
+      }),
+      bundleSize(),
+    ],
+  },
+];
